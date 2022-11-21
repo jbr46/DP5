57eeb66c52cc50f1ae4cfd62d499644389dedf9e
Benji Rowlands <jbr46@cam.ac.uk>
2022-11-21 16:23:12 +0000

    Update functions in SGNN_DP5.py for calling the SGNN workflow

--- SGNN_DP5.py
+++ SGNN_DP5.py
@@ -21,43 +21,10 @@ def SetupNMRPred(Isomers, settings):
     with open('SGNN_inputs.csv', 'w') as inputfile:
         inputfile.write(',sample-id,SMILES\n')
         for num, iso in enumerate(Isomers):
-            if os.path.exists(filename + '.sout'):
-                iso.SGNNOutputFiles.append(filename + '.sout')
+            if os.path.exists(inputfile + '.sout'):
+                iso.SGNNOutputFiles.append(inputfile + '.sout')
             else:
-                inputfile.write(str(num) + ',HMDB' + str(num) + ',' + iso.Smiles + '\n')
-        
-
-    # for iso in Isomers:
-    #     if iso.ExtCharge > -10:
-    #         charge = iso.ExtCharge
-    #     else:
-    #         charge = iso.MMCharge
-
-        # if iso.DFTConformers == []:
-        #     conformers = iso.Conformers
-        # else:
-        #     conformers = iso.DFTConformers
-
-        # filename = iso.BaseName + '.sinp'
-
-        # Checks for pre-existing SGNN output files and assumes they are done if they exist
-        # if os.path.exists(filename + '.sout'):
-        #     iso.SGNNOutputFiles.append(filename + '.sout')
-        # else:    
-        #     iso.SGNNInputFiles.append(filename + '.scom')
-
-        # for num in range(0, len(conformers)):
-        #     filename = iso.BaseName + 'ginp' + str(num + 1).zfill(3)
-
-        #     if os.path.exists(filename + '.out'):
-        #         if IsGausCompleted(filename + '.out'):
-        #             iso.NMROutputFiles.append(filename + '.out')
-        #             continue
-        #         else:
-        #             os.remove(filename + '.out')
-
-        #     WriteGausFile(filename, conformers[num], iso.Atoms, charge, settings, 'nmr')
-        #     iso.NMRInputFiles.append(filename + '.com')
+                inputfile.write(str(num) + ',' + iso.BaseName + ',' + iso.Smiles + '\n')
 
     os.chdir(jobdir)
 
@@ -71,131 +38,39 @@ def RunNMRPred(Isomers, settings):
     jobdir = os.getcwd()
     os.chdir('nmr')
 
-    # SGNNJobs = []
-
-    # for iso in Isomers:
-    #     SGNNJobs.extend([iso.BaseName if (iso.BaseName + '.sout') not in iso.SGNNOutputFiles])
-
-    Completed = Pred(settings)
-
-    for iso in Isomers:
-        iso.SGNNOutputFiles.extend([iso.BaseName + '.sout' if (iso.BaseName + '.sout') in Completed])
+    SGNN_pred(settings)
 
     os.chdir(jobdir)
 
     return Isomers
 
 
-def GetPrerunNMRPred(Isomers):
+# def GetPrerunNMRPred(Isomers):
 
-    print('\nLooking for prerun SGNN NMR prediction files...')
+#     print('\nLooking for prerun SGNN NMR prediction files...')
 
-    jobdir = os.getcwd()
-    os.chdir('nmr')
+#     jobdir = os.getcwd()
+#     os.chdir('nmr')
 
-    for iso in Isomers:
-        iso.NMRInputFiles = glob.glob(iso.BaseName + 'sinp*scom')
-        iso.NMROutputFiles.extend([x[:-4] + '.sout' for x in iso.NMRInputFiles if IsGausCompleted(x[:-4] + '.sout')])
+#     for iso in Isomers:
+#         iso.NMRInputFiles = glob.glob(iso.BaseName + 'sinp*scom')
+#         iso.NMROutputFiles.extend([x[:-4] + '.sout' for x in iso.NMRInputFiles if IsGausCompleted(x[:-4] + '.sout')])
 
-    print('NMR prediction files:')
-    print(', '.join([', '.join(x.NMROutputFiles) for x in Isomers]))
+#     print('NMR prediction files:')
+#     print(', '.join([', '.join(x.NMROutputFiles) for x in Isomers]))
 
-    os.chdir(jobdir)
+#     os.chdir(jobdir)
 
-    return Isomers
+#     return Isomers
 
 
-def Pred(SGNNJobs, settings):
+def SGNN_pred(settings):
 
-    # TODO: implement logic to run SGNN predictions
+    from SGNN import prediction
     
-    if len(SGNNJobs) == 0:
-        print("There were no jobs to run.")
-        return Completed
-
-    print("Function to run the SGNN predictions not yet implemented. Quitting.")
-
-    quit()
-
-    # NCompleted = 0
-    # Completed = []
-
-
-    # if ('GAUS_EXEDIR' in os.environ):
-    #     gausdir = os.environ['GAUSS_EXEDIR']
-    #     if shutil.which(os.path.join(gausdir, 'g09')) is None:
-    #         GausPrefix = os.path.join(gausdir, "g16")
-    #     else:
-    #         GausPrefix = os.path.join(gausdir, "g09")
-    # else:
-    #     GausPrefix = settings.GausPath
-
-    # if shutil.which(GausPrefix) is None:
-    #     print('Gaussian.py, RunCalcs:\n  Could not find Gaussian executable at ' + GausPrefix)
-    #     quit()
-
-    # for f in GausJobs:
-    #     time.sleep(3)
-    #     print(GausPrefix + " < " + f + ' > ' + f[:-3] + 'out')
-    #     outp = subprocess.check_output(GausPrefix + " < "  + f + ' > ' + f[:-3] + 'out', shell=True,timeout= 86400)
-
-    #     NCompleted += 1
-    #     if IsGausCompleted(f[:-4] + '.out'):
-    #         Completed.append(f[:-4] + '.out')
-    #         print("Gaussian job " + str(NCompleted) + " of " + str(len(GausJobs)) + \
-    #               " completed.")
-    #     else:
-    #         print("Gaussian job terminated with an error. Continuing.")
-
-    # if NCompleted > 0:
-    #     print(str(NCompleted) + " Gaussian jobs completed successfully.")
-
-    # return Completed
-
-
-# def WriteSGNNFile(SGNNinp, atoms, charge, settings, type):
-
-#     f = open(SGNNinp + '.scom', 'w')
-#     if(settings.nProc > 1):
-#         f.write('%nprocshared=' + str(settings.nProc) + '\n')
-#     if settings.DFT == 'g':
-#         f.write('%mem=2000MB\n%chk='+Gausinp + '.chk\n')
-#     elif settings.DFT == 'z':
-#         f.write('%mem=1000MB\n%chk=' + Gausinp + '.chk\n')
-#     else:
-#         f.write('%mem=6000MB\n%chk='+Gausinp + '.chk\n')
-
-#     if type == 'nmr':
-#         f.write(NMRRoute(settings))
-#     elif type == 'e':
-#         f.write(ERoute(settings))
-#     elif type == 'opt':
-#         f.write(OptRoute(settings))
-
-#     f.write('\n'+Gausinp+'\n\n')
-#     f.write(str(charge) + ' 1\n')
-
-#     natom = 0
-
-#     for atom in conformer:
-#         f.write(atoms[natom] + '  ' + atom[0] + '  ' + atom[1] + '  ' +
-#                 atom[2] + '\n')
-#         natom = natom + 1
-#     f.write('\n')
-
-#     f.close()
-
-
-# def IsGausCompleted(f):
-#     Gfile = open(f, 'r')
-#     outp = Gfile.readlines()
-#     Gfile.close()
-#     if len(outp) < 10:
-#         return False
-#     if ("Normal termination" in outp[-1]) or (('termination' in '\n'.join(outp[-3:])) and ('l9999.exe' in '\n'.join(outp[-3:]))):
-#         return True
-#     else:
-#         return False
+    save_folder = os.getcwd()
+    path_csv = save_folder + 'SGNN_inputs.csv'
+    prediction(save_folder, path_csv)
 
 
 def ReadPred(Isomers):
@@ -204,18 +79,10 @@ def ReadPred(Isomers):
     os.chdir('nmr')
 
     for iso in Isomers:
-
-        if len(iso.SGNNOutputFiles) < 1:
-            print("SGNN.py, ReadPred: No NMR SGNN output" +
-                  " files found, NMR data could not be read. Quitting.")
-            quit()
-
-        for OutpFile in iso.NMROutputFiles:
-
-            with open(OutpFile, 'r') as sgnnfile:
-                for shift in sgnnfile:
-                    iso.PredShifts.append(shift)
-
+        output_file = iso.BaseName + '.sout'
+        with open(output_file, 'r') as sgnnfile:
+            for shift in sgnnfile:
+                iso.PredShifts.append(shift)
 
     os.chdir(jobdir)
 
